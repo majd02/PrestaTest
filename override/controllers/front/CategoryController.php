e294ac3507ec9dd627bb509a330b07029ac1071e
majd02 <majdidani02@gmail.com>
2025-07-21 21:12:57 +0100

    Task3

--- override/controllers/front/CategoryController.php
+++ override/controllers/front/CategoryController.php
@@ -21,53 +21,6 @@ class CategoryController extends CategoryControllerCore
     {
         return (int)Product::getCategoryProductsCount($id_category);
     }
-    public function getProductList()
-    {
-        $originalProductList = parent::getProductList();
-
-        $modifiedProducts = [];
-
-        foreach ($originalProductList['products'] as $product) {
-            // Load all combinations for this product
-            $combinations = Product::getProductAttributesIds($product['id_product']);
-            $combinationImages = Product::getCombinationImages((int)Context::getContext()->language->id);
-
-            if ($combinations) {
-                foreach ($combinations as $combination) {
-                    $combinationId = $combination['id_product_attribute'];
-                    $combData = new Combination($combinationId);
-                    $productCopy = $product;
-
-                    // Assign combination image
-                    $imageId = null;
-                    if (isset($combinationImages[$product['id_product']][$combinationId])) {
-                        $imageId = $combinationImages[$product['id_product']][$combinationId][0]['id_image'];
-                    }
-
-                    if ($imageId) {
-                        $productCopy['id_image'] = $imageId;
-                        $productCopy['id_product_attribute'] = $combinationId;
-                    }
-
-                    // Optional: append color name to product name
-                    $attributes = $combData->getAttributesName((int)Context::getContext()->language->id);
-                    $attributeNames = [];
-                    foreach ($attributes as $attr) {
-                        $attributeNames[] = $attr['name'];
-                    }
-                    $productCopy['name'] .= ' - ' . implode(' ', $attributeNames);
-
-                    $modifiedProducts[] = $productCopy;
-                }
-            } else {
-                // No combinations: keep the original product
-                $modifiedProducts[] = $product;
-            }
-        }
-
-        // Replace original product list
-        $originalProductList['products'] = $modifiedProducts;
-        return $originalProductList;
-    }
+   
    }
 
