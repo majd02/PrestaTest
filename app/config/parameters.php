2bf26b4491faa422caf5e6a04235bcb2f51abb16
majd02 <majdidani02@gmail.com>
2025-07-21 20:56:37 +0100

    initial commit

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'prestashop2',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => '74ZhN4MwH9VrqKvJvHi45m1b8jCkzDN0on1MhEaUEdSbcLHjVy6GZcjo',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2025-07-21',
+    'locale' => 'en-US',
+    'cookie_key' => 'cotWhthTsZRBhqGoaH29jYSH0itfiKqC0gBikhR9VFrNAzNoDNphWfDj',
+    'cookie_iv' => 'gB18e64I',
+    'new_cookie_key' => 'def0000034763200719565ea45a358c789256c0a4aa2d44a2f9cc13ad8029693572567a1c37905b41d59bf6523c69bd3aaeee4bed1e74c50baeb54e6d2e2b645fbca628a',
+  ),
+);
